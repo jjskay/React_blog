702195cfa6161aca0043f5533b4a232b1b62c277
jjskay <404784102@qq.com>
2015-12-08 23:53:37 +0800

    Test material-ui 01.

--- src/components/UI/PageHead.jsx
+++ src/components/UI/PageHead.jsx
@@ -5,14 +5,15 @@ var { Route, RouteHandler, Link } = Router;
 
 var ReactIntl = require('react-intl');
 var { IntlMixin, FormattedMessage } = ReactIntl;
-var {Menu, MenuItem, MenuDivider, Avatar, AppBar, RaisedButton} = require('material-ui/lib');
+var {Menu, MenuItem, MenuDivider, Avatar, AppBar, RaisedButton, FontIcon} = require('material-ui/lib');
 
 var AuthStore = require('../../stores/AuthStore');
 var AuthActions = require('../../actions/AuthActions');
 
 var PageHead = React.createClass({
 	contextTypes: {
-		router: React.PropTypes.func.isRequired
+		router: React.PropTypes.func.isRequired,
+		muiTheme: React.PropTypes.object,
 	},
 
 	mixins: [IntlMixin,FluxibleMixin],
@@ -23,12 +24,16 @@ var PageHead = React.createClass({
 
 	getInitialState: function(){
 		return this.getStateFromStores();
+
 	},
 
 	getStateFromStores: function(){
+		// <AppBar title="My AppBar" />
+  //       		<RaisedButton label="My Button" primary={true} />
 		return {
 			user: this.getStore(AuthStore).getUser(),
-			inserStatus: this.getStore(AuthStore).getInsertStatus()
+			inserStatus: this.getStore(AuthStore).getInsertStatus(),
+			muiTheme: this.getStore(AuthStore).getInsertStatus(),
 		}
 	},
 
@@ -37,11 +42,15 @@ var PageHead = React.createClass({
 	},
 
 	render: function(){
+		var data = {
+			color: '#666',
+		    hoverColor: "red",
+		    onMouseLeave: function(){console.log("Leave");},
+		    onMouseEnter: function(){console.log("Enter");},
+		    style: {width:100}
+		}
         return (
-			<div>
-                <AppBar title="My AppBar" />
-        		<RaisedButton label="My Button" primary={true} />
-			</div>
+        	<div><FontIcon className="muidocs-icon-action-home" {...data}/></div>
         )
 	},
 })
